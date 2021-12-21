782d78010bd0c49ecdf64ff3d5e154638f93f5ba
ms5676.shin <alstn5038@gmail.com>
2021-12-21 20:49:38 +0900

    Add comments

--- ps/week07_day2_디스크컨트롤러.py
+++ ps/week07_day2_디스크컨트롤러.py
@@ -1,24 +1,23 @@
 # 통과 
 # 다른사람 풀이
 
-# 하드디스크 한번에 하나의 작업만
-# 요청이 들어온 순서대로
-# 누적합이 가장 작은
-
 def solution(jobs):
-    answer = 0
+    answer = 0 
     time = 0
     n = len(jobs)
+    # 소요시간이 적게 걸리는 작업부터
     jobs = sorted(jobs, key=lambda x: x[1])
-
+    # 작업이 없을때까지 계속 진행
     while jobs:
+        # 모든 작업에 대해서
         for i in range(len(jobs)):
+            # time보다 낮은 작업이면서 적은 소요시간 작업 진행 
             if jobs[i][0] <= time:
                 time += jobs[i][1]
                 answer += time - jobs[i][0]
                 jobs.pop(i)
                 break
-            
+            # 마지막까지 확인했는데 없다면, time+=1 
             if i == len(jobs) - 1:
                 time += 1
 
