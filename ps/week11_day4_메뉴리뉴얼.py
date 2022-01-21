e598046eec7d514115a4dfdb70b3efd9587204d3
ms5676.shin <alstn5038@gmail.com>
2022-01-21 13:21:14 +0900

    update

--- ps/week11_day4_메뉴리뉴얼.py
+++ ps/week11_day4_메뉴리뉴얼.py
@@ -13,23 +13,20 @@
 # 'XWY' 기준에서 'WX'
 # defaultdict(int) 갯수를 올려준다. 
     # key를 만들떈 ''.join(sorted(list))
-# same = [#,#,{}, ] {'XY':2, 'WX':2}
+# candidates = [#,#,{}, ] {'XY':2, 'WX':2}
 #         0 1 2 3
 
 # for문을 돌면서 max값이 되면 max_list를 비워주고 
 # max_list가 만들어 졌으면 answer에 extend하면 된다. 
-
-from itertools import combinations
 from collections import defaultdict
+from itertools import combinations, permutations
 
 def solution(orders, course):
     answer = []
-    # same = defaultdict(int)
-    # courses = [defaultdict(int)] * (course[-1]+1) # coures[-1] : 찾아야하는 코스 요리 수의 최대값
-    courses = [defaultdict(int) for _ in range(course[-1] + 1)]
+    courses = [defaultdict(int) for _ in range(course[-1]+1)] # coures[-1] : 찾아야하는 코스 요리 수의 최대값
 
-    # 두 문자열을 비교해가면서 공통 문자들을 구하고
-    # permutation으로 돌린다음 각 낱개에 대해 dict에 갯수를 반영해준다. 
+    # 두 문자열을 비교해가면서 공통 가능한 조합들을 구한다. 
+    candidates = []
     for i in range(len(orders)):
         for j in range(i+1, len(orders)):
             w1, w2 = orders[i], orders[j]
@@ -37,18 +34,32 @@ def solution(orders, course):
             for word in w1:
                 if word in w2:
                     str_list.append(word)
-            # print(str_list)
-            if len(str_list) >= 2:
-                print(str_list)
-                for k in range(2, len(str_list)+1):
-                    for perm in combinations(str_list, k):
-                        print(k, perm)
-                        perm = ''.join(sorted(perm))
-                        courses[k][perm] += 1
-    print(courses)      
-    # course를 돌면서 원하는 코스 요리수별
-    # same에서 가장 많이 선택한 코스요리를 찾는다. 
-        # dict의 value기준으로 정렬해서 가장 큰 값을 고른다. 
+            for k in range(2, len(str_list)+1):
+                for perm in combinations(str_list, k):
+                    candidates.append(''.join(sorted(perm)))
+    candidates = list(set(candidates))
+    # candidates = ['ACDE', 'BCFG', 'CDE', 'AC']
+
+    #'AC': cnt, 'CDE' : cnt, 'ACDE': cnt, 'BCFG' : cnt
+
+    # candidates = ['ACDE', 'BCFG', 'CDE', 'AC']
+    for candidate in candidates:
+        len_candi = len(candidate)
+        # 원하는 course의 수가 아닐 경우 pass
+        if len_candi not in course: continue
+        
+        # orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+        # candidate가 orders에 포함되어있는 수를 계산해서 courses에 저장한다. 
+        for i in range(len(orders)):
+            cnt = 0
+            for one_elem in candidate:     # candidate = 'ACDE', one_elem = 'A'
+                if one_elem in orders[i]:
+                    cnt += 1
+            if cnt == len_candi:
+                courses[len_candi][candidate] += 1
+    
+    # course를 돌면서 원하는 코스 요리의 수마다 가장 많이 선택한 조합을 answer에 저장
+    #     dict의 value기준으로 정렬해서 가장 큰 값을 고른다. 
     for c in course:
         temp_dict = courses[c]
         max_course = []
@@ -64,4 +75,7 @@ def solution(orders, course):
     return sorted(answer)
 
 
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
+print(solution(["ABCD", "ABCD", "ABCD"], [2,3,4]))
+
