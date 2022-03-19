cce7ea80c46a70f9e202a3f6b4c2efba9659f4a3
Minsoo-Shin <alstn5038@gmail.com>
2022-03-20 03:17:57 +0900

    initial

--- ps_after/pgm_문자열압축.py
+++ ps_after/pgm_문자열압축.py
@@ -1,12 +1,13 @@
 def solution(s):
     # 자르는 unit
-    for unit in range(1, len(s)//2):
-        answer = float('inf')
+    if len(s) == 1:
+        return 1
+    answer = float('inf')
+    for unit in range(1, (len(s)//2)+1):
         temp_mul = 1
         prior = ''
         result = ''
         for i in range(0, len(s), unit):
-            print(i, i+unit)
             if s[i:i+unit] == prior:
                 temp_mul += 1
             else:
@@ -14,15 +15,13 @@ def solution(s):
                     result += prior
                 else:
                     result += str(temp_mul) + prior
-
                 prior = s[i:i+unit]
                 temp_mul = 1
-            if i == len(s) - unit:
+            if i >= len(s) - unit:
                 if temp_mul == 1:
                     result += prior
                 else:
                     result += str(temp_mul) + prior
-        print(result)
         answer = min(answer, len(result))
         
     return answer
